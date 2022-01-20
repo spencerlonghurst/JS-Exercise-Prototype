bc95a12a7978e4dbf8fdf633cb692bd8410650c5
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-01-19 17:01:10 -0700

    Finished project

--- index.js
+++ index.js
@@ -39,22 +39,58 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
 }
 
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
 
+//create new people
 
+const sallie = new Person('Sallie', 26);
+const mark = new Person('Mark', 23);
+const cori = new Person('Cori', 25);
+
+sallie.toString();
+mark.toString();
+cori.toString();
+
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+mark.eat('🍔');
+
+//console.log(mark.stomach);
+
+mark.poop();
+
+//console.log(mark.stomach);
 
 
 
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with an `tank` at 0
-        + should initialize with an `odometer` at 0
+    X - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    X - All instances built with Car:
+        X + should initialize with an `tank` at 0
+        X + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
@@ -63,30 +99,74 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons; 
+}
+
+//create a new car
+const truck = new Car('Dodge', 15);
+
+
+//console.log('task 2', truck.fill(10)); //Add the gallons to 'tank'
+
+
+
+
+
 
 
 /*
   TASK 3
-    - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
+    X- Write a Baby constructor subclassing Person.
+    X- Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    X - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
+
+
+const babyOne = new Baby('Jimmy', 1, 'blocks');
+
+console.log(babyOne.play());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global scope: When this is used in the global scope, the console won't know what specifically you are requesting the 'this' on, so it will just simply return the entire window.
+  2. Implicit Binding: When Implicit binding is used, when you simply look to the left of the dot, you can determine what 'this' is referring to.
+  3. Explicit Binding: Whenever you use .call, .apply or .bind you are using explicit binding.
+  4. New Binding: When you are using a constructor function, this is used with the 'new' keyword.
 */
 
 
